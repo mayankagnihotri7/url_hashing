649f2dd0ffd7119df521009e52d57664971fbb94
Mayank <mayankagnihotri7@gmail.com>
2022-02-05 14:03:10 +0530

    Added react as frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root "home#index"
 
   resources :links, only: %i[index create update show]
 
-  # get "*path", to: "home#index", via: :all
+  get "*path", to: "home#index", via: :all
 end
